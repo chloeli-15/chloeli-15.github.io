5b94586a50e3baeeb20282b5e26164d31f805164
Chloe Li <chloeli561@gmail.com>
2024-08-07 01:44:22 +0100

    changing plugin for list photos

--- _plugins/list_photos.rb
+++ _plugins/list_photos.rb
@@ -8,8 +8,16 @@ module Jekyll
         art_photos_dir = File.join(site.source, 'assets/images/art')
   
         # Read the files from the directories
-        site.data['cat_photos'] = Dir.children(cat_photos_dir).select { |f| File.file?(File.join(cat_photos_dir, f)) }
-        site.data['art_photos'] = Dir.children(art_photos_dir).select { |f| File.file?(File.join(art_photos_dir, f)) }
+        cat_photos = Dir.children(cat_photos_dir).select { |f| File.file?(File.join(cat_photos_dir, f)) }
+        art_photos = Dir.children(art_photos_dir).select { |f| File.file?(File.join(art_photos_dir, f)) }
+  
+        # Output to the console for debugging
+        puts "Cat Photos: #{cat_photos.inspect}"
+        puts "Art Photos: #{art_photos.inspect}"
+  
+        # Assign to site data
+        site.data['cat_photos'] = cat_photos
+        site.data['art_photos'] = art_photos
       end
     end
   end
