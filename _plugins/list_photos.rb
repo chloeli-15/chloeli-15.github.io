8d559ecfb3f05ee9ac37b698125f1de474e2ed74
Chloe Li <chloeli561@gmail.com>
2024-08-07 01:48:52 +0100

    gallery changes for no cat photos

--- _plugins/list_photos.rb
+++ _plugins/list_photos.rb
@@ -7,17 +7,18 @@ module Jekyll
         cat_photos_dir = File.join(site.source, 'assets/images/cats')
         art_photos_dir = File.join(site.source, 'assets/images/art')
   
-        # Read the files from the directories
-        cat_photos = Dir.children(cat_photos_dir).select { |f| File.file?(File.join(cat_photos_dir, f)) }
-        art_photos = Dir.children(art_photos_dir).select { |f| File.file?(File.join(art_photos_dir, f)) }
+        # Read the files from the directories and handle common image extensions
+        site.data['cat_photos'] = read_images(cat_photos_dir)
+        site.data['art_photos'] = read_images(art_photos_dir)
   
         # Output to the console for debugging
-        puts "Cat Photos: #{cat_photos.inspect}"
-        puts "Art Photos: #{art_photos.inspect}"
+        puts "Cat Photos: #{site.data['cat_photos'].inspect}"
+        puts "Art Photos: #{site.data['art_photos'].inspect}"
+      end
   
-        # Assign to site data
-        site.data['cat_photos'] = cat_photos
-        site.data['art_photos'] = art_photos
+      def read_images(dir)
+        return [] unless Dir.exist?(dir)
+        Dir.children(dir).select { |f| f =~ /\.(jpg|jpeg|png|gif)$/i && File.file?(File.join(dir, f)) }
       end
     end
   end
